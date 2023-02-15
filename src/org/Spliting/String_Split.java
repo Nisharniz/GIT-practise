d21d630b133e9e1b7b0b6f717443a901fa17f8ea
Nishar <mailtonisharniz@gmailcom>
2023-02-15 12:46:45 +0530

    Programming session

---
+++ src/org/Spliting/String_Split.java
@@ -0,0 +1,19 @@
+package org.Spliting;
+
+public class String_Split {
+
+	public static void main(String[] args) {
+
+		int a = 18;
+
+		if (a < 18) {
+
+			System.out.println("Eligible to vote");
+		} 
+		else {
+			System.out.println("not eligible to vote");
+
+		}
+
+	}
+}
